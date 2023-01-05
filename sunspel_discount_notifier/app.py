a1d5fd84be228db8b0e7ca3beb99d4d4beccdf68
blee1077 <superbrandonlee@hotmail.com>
2023-01-05 00:27:43 +0000

    Fixed broken URL links in HTML table of email

--- sunspel_discount_notifier/app.py
+++ sunspel_discount_notifier/app.py
@@ -34,7 +34,7 @@ def load_json(bucket: str, key: str):
     return json.loads(file_content)
 
 
-def convert_dict_to_dataframe(prod_dict: dict):
+def convert_dict_to_dataframe(prod_dict: dict, convert_dict: dict):
     """Converts a dictionary into a Pandas DataFrame.
     
     Args:
@@ -43,13 +43,6 @@ def convert_dict_to_dataframe(prod_dict: dict):
     Returns:
         Pandas DataFrame
     """
-    # Specify the dtypes of columns to cast
-    convert_dict = {
-        'url': str,
-        'price': float,
-        'colour': str
-    }
-
     # Convert the dict into a dataframe
     prod_df = pd.DataFrame.from_records(prod_dict).T.reset_index()
     prod_df = prod_df.drop(columns=['index'])
@@ -59,7 +52,7 @@ def convert_dict_to_dataframe(prod_dict: dict):
         assert set(prod_df.columns).intersection(convert_dict.keys()) == set(convert_dict.keys())
     except AssertionError as e:
         missing_keys = set(convert_dict.keys()).difference(prod_dict.keys())
-        logger.info(f"Missing following keys in scraped product dictionary: {', '.join(missing_keys)}")
+        logger.error(f"Missing following keys in scraped product dictionary: {', '.join(missing_keys)}")
         raise e
 
     # Cast columns
@@ -114,9 +107,9 @@ def lambda_handler(event, context):
     MIN_DISCOUNT_PERC = int(os.environ['MIN_DISCOUNT_PERC'])
 
     # Define base URL and get all products from the page
-    base_url = "https://www.sunspel.com/"
-    riv_polo_url = "uk/mens/polo-shirts/riviera-polo.html"
-    page = requests.get(base_url+riv_polo_url, headers = {'User-agent': 'tmp'})
+    BASE_URL = "https://www.sunspel.com/"
+    RIV_POLO_URL = "uk/mens/polo-shirts/riviera-polo.html"
+    page = requests.get(BASE_URL+RIV_POLO_URL, headers = {'User-agent': 'tmp'})
     soup = BeautifulSoup(page.content, "html.parser")
     product_list = soup.find_all("li", class_="prd-List_Item")
     assert len(product_list) > 0, "No products found from HTML, web page has changed."
@@ -129,7 +122,7 @@ def lambda_handler(event, context):
         try:
             prod_type = prod.find("h3", class_="prd-Card_Title").get_text().strip()
         except KeyError as e:
-            logger.info("Unable to get product type from product HTML, web page has changed.")
+            logger.error("Unable to get product type from product HTML, web page has changed.")
             raise e
         
         # If it isn't the one I want then move on
@@ -140,19 +133,21 @@ def lambda_handler(event, context):
         try:
             prod_id = int(prod['id'].split('-')[-1])
         except KeyError as e:
-            logger.info("Unable to get product ID from product HTML, web page has changed.")
+            logger.error("Unable to get product ID from product HTML, web page has changed.")
             raise e
 
         try:
             prod_colour = prod.find("p", class_="prd-Card_Colour").get_text().strip()
         except KeyError as e:
-            logger.info("Unable to get product colour from product HTML, web page has changed.")
+            logger.error("Unable to get product colour from product HTML, web page has changed.")
             raise e
 
         try:
-            prod_url = base_url + prod.find("a", class_="util-FauxLink_Link")['href']
+            prod_url = BASE_URL[:-1] + prod.find("a", class_="util-FauxLink_Link")['href']
+            if '.html' not in prod_url:
+                prod_url = prod_url + '.html'
         except Exception as e:
-            logger.info("Unable to get product URL from product HTML, web page has changed.")
+            logger.error("Unable to get product URL from product HTML, web page has changed.")
             raise e
 
         try:
@@ -165,42 +160,51 @@ def lambda_handler(event, context):
                     )
                 ).split('?')[0]
         except Exception as e:
-            logger.info("Unable to get product image URL from product HTML, web page has changed.")
+            logger.error("Unable to get product image URL from product HTML, web page has changed.")
             raise e
 
         try:
             # Attempt to get Sale price
             sale_price_lst = prod.find('p', class_='prd-Card_Price').find('span', class_='prd-Card_SalePrice').get_text().split('\n')
-            prod_price = float([sale_price_str.strip() for sale_price_str in sale_price_lst if len(sale_price_str.strip()) > 0 and '£' in sale_price_str][0].replace('£', ''))
+            prod_price = float(
+                [sale_price_str.strip() for sale_price_str in sale_price_lst
+                    if len(sale_price_str.strip()) > 0 and '£' in sale_price_str][0]
+                .replace('£', '')
+            )
         except AttributeError as e:
             logger.info("Couldn't get sale price, attempting to get regular price.")
             try:
                 prod_price = float(prod.find('p', class_='prd-Card_Price').get_text().replace('£', ''))
             except Exception as e:
-                logger.info("Unable to get product price from product HTML, web page has changed.")
+                logger.error("Unable to get product price from product HTML, web page has changed.")
                 raise e
         except Exception as e:
-            logger.info("Unable to get product price from product HTML, web page has changed.")
+            logger.error("Unable to get product price from product HTML, web page has changed.")
             raise e
         
         # Store in dictionary
         prod_dict[prod_id] = {}
-        prod_dict[prod_id]['url'] = prod_url
-        prod_dict[prod_id]['img_url'] = prod_img_url
-        prod_dict[prod_id]['price'] = prod_price
-        prod_dict[prod_id]['colour'] = prod_colour
+        prod_dict[prod_id]['Link URL'] = prod_url
+        prod_dict[prod_id]['Image'] = prod_img_url
+        prod_dict[prod_id]['Price (£)'] = prod_price
+        prod_dict[prod_id]['Colour'] = prod_colour
     
     # Convert product dictionary into dataframe
-    df = convert_dict_to_dataframe(prod_dict)
+    convert_dict = {
+        'Link URL': str,
+        'Price (£)': float,
+        'Colour': str
+    }
+    df = convert_dict_to_dataframe(prod_dict, convert_dict)
     
     # Calculate discount
-    df['discount (%)'] = (100 * (1- (df['price'] / df['price'].max()))).round(2)
+    df['Discount (%)'] = (100 * (1- (df['Price (£)'] / df['Price (£)'].max()))).round(2)
     
     # Get sizes in-stock for each product
-    df['sizes available'] = df['url'].apply(lambda x: ', '.join(size_availability(x)))
+    df['Available Sizes'] = df['Link URL'].apply(lambda x: ', '.join(size_availability(x)))
     
     # Calculate how many products are above the minimum discount threshold
-    abv_disc_thresh = (df['discount (%)'] >= MIN_DISCOUNT_PERC).sum()
+    abv_disc_thresh = (df['Discount (%)'] >= MIN_DISCOUNT_PERC).sum()
     
     # Only if there's products above threshold then send an email
     if abv_disc_thresh > 0:
@@ -215,7 +219,7 @@ def lambda_handler(event, context):
         """.format(
             MIN_DISCOUNT_PERC,
             build_table(
-                df[df['discount (%)']>=MIN_DISCOUNT_PERC].sort_values(by='discount (%)', ascending=False),
+                df[df['Discount (%)']>=MIN_DISCOUNT_PERC].sort_values(by='Discount (%)', ascending=False),
                 'blue_light'
             )
         )
@@ -241,7 +245,7 @@ def lambda_handler(event, context):
     
         # Send email
         email.send(
-            subject=f"Sunspel has discounts up to {int(df['discount (%)'].max())}% on Riviera polo shirts!",
+            subject=f"Sunspel has discounts up to {int(df['Discount (%)'].max())}% on Riviera polo shirts!",
             sender=email_secret['sender_email'],
             receivers=[email_secret['receiver_email']],
             html=html
